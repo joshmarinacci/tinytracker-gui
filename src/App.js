9e44c7ee969047129f9c3affb99990b3dc9f4a50
Josh Marinacci <joshua@marinacci.org>
2020-01-16 09:53:23 -0800

    initial import

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react'
+import './App.css'
+import {AuthModuleSingleton, LOGIN} from './auth.js'
+
+const AUTH_URL = 'http://vr.josh.earth:3965/github'
+const DATA_URL = 'http://vr.josh.earth:3965/data.json'
+
+const auth = new AuthModuleSingleton()
 
 function App() {
+  const [loggedin, setLoggedin] = useState(auth.isLoggedIn())
+  const login = () => auth.login(AUTH_URL)
+  const logout = () => auth.logout()
+  useEffect(()=>{
+    const cb = () => setLoggedin(auth.isLoggedIn())
+    auth.on(LOGIN,cb)
+    return ()=>auth.off(LOGIN,cb)
+  })
+
+  const loadData = () => {
+    auth.fetch(DATA_URL,{method:'GET'})
+        .then(d => d.json())
+        .then(data=>{
+      console.log('got the data',data)
+    })
+  }
+  let button = <button onClick={login}>login</button>
+  if(loggedin) button = <button onClick={logout}>log out</button>
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {button}
+      <button onClick={loadData}>data</button>
     </div>
   );
 }
